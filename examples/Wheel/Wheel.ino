94f6cf38086b82365eaec6dcf588e03aed6363f2
saces <saces@saces.de>
2014-03-19 21:28:26 +0100

    add wheel mouse support

---
+++ examples/Wheel/Wheel.ino
@@ -0,0 +1,40 @@
+/**
+ * Reads X/Y values from a PS/2 mouse connected to an Arduino
+ * using the PS2Mouse library available from
+ *   http://github.com/kristopher/PS2-Mouse-Arduino/
+ * Original by Kristopher Chambers <kristopher.chambers@gmail.com>
+ * Updated by Jonathan Oxer <jon@oxer.com.au>
+ */
+
+#include <PS2Mouse.h>
+#define MOUSE_DATA 4
+#define MOUSE_CLOCK 2
+
+PS2IMouse mouse(MOUSE_CLOCK, MOUSE_DATA, REMOTE);
+
+/**
+ * Setup
+ */
+void setup()
+{
+  Serial.begin(38400);
+  mouse.initialize();
+}
+
+/**
+ * Main program loop
+ */
+void loop()
+{
+  int data[4];
+  mouse.report(data);
+  Serial.print(data[0]); // Status Byte
+  Serial.print(":");
+  Serial.print(data[1]); // X Movement Data
+  Serial.print(",");
+  Serial.print(data[2]); // Y Movement Data
+  Serial.print(",");
+  Serial.print(data[3]); // Z Movement Data (wheel)
+  Serial.println();
+  delay(20);
+}
